fed2c683455b4df0419244fbdd4f22c45bf25033
crypto-jones <integral.being@gmail.com>
2019-01-07 13:38:51 -0800

    Set up hrefs for sites and emails

--- gatsby-starter-strata/src/components/Footer.js
+++ gatsby-starter-strata/src/components/Footer.js
@@ -1,23 +1,54 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div id="footer">
-                <div className="inner">
-                    <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="footer">
+        <div className="inner">
+          <ul className="icons">
+            <li>
+              <a
+                href="https://github.com/crypto-jones"
+                target="_blank"
+                className="icon fa-github"
+              >
+                <span className="label">Github</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/jeremyalexanderjones/"
+                target="_blank"
+                className="icon fa-linkedin"
+              >
+                <span className="label">Linkedin</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://medium.com/@jeremy.a.jones"
+                className="icon fa-medium"
+                target="_blank"
+              >
+                <span className="label">Medium</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:jeremyjones.a@gmail.com"
+                className="icon fa-envelope-o"
+              >
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+          <ul className="copyright">
+            <li>&copy; Jeremy A. Jones</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
